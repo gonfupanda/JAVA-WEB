ad25e70dc293134d27ec792ac8bff787d26f425f
gonfupanda <gonpadierna18@gmail.com>
2022-10-25 12:13:06 +0200

    java con server

---
+++ UF2218Gonza/src/main/java/java/com/ipartek/formacion/gonza_uf2218/Hola.java
@@ -0,0 +1,44 @@
+package java.com.ipartek.formacion.gonza_uf2218;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class Hola
+ */
+@WebServlet("/saludo")
+public class Hola extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Hola() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nombre = request.getParameter("nombre");
+				
+		response.setContentType("text/plain");
+		response.getWriter().println("Hola " + nombre);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
