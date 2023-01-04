48fa9f81f1a87738eb9d7779105362534d099745
gonfupanda <gonpadierna18@gmail.com>
2023-01-04 12:00:48 +0100

    carrito funcional+seguridad+login casi

--- demo/src/main/java/com/ipartek/formacion/spring/controladores/admin/IndexController.java
+++ demo/src/main/java/com/ipartek/formacion/spring/controladores/admin/IndexController.java
@@ -5,10 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.ipartek.formacion.spring.pojos.Pedido;
+import com.ipartek.formacion.spring.service.CarritoService;
 import com.ipartek.formacion.spring.service.ProductoService;
 
 @Controller
@@ -16,16 +20,43 @@ import com.ipartek.formacion.spring.service.ProductoService;
 @SessionAttributes("carrito")
 public class IndexController {
 
-	@Autowired
-	private ProductoService servicio;
-
-	@GetMapping
-	public String index(Model modelo) {
-		modelo.addAttribute("productos", servicio.obtenerTodos());
-		return "index";
-	}
 	@ModelAttribute("carrito")
 	public Pedido getCarrito() {
 		return new Pedido();
+	}
+	
+	@Autowired
+	private ProductoService productoService;
+	
+	@Autowired
+	private CarritoService carritoService;
+
+	@GetMapping
+	public String index(Model modelo) {
+		modelo.addAttribute("productos", productoService.obtenerTodos());
+		return "index";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/carrito")
+	public String verCarrito() {
+		return "carrito";
+	}
+	
+	@PostMapping("/carrito")
+	public String agregarProductoACarrito(Long id, Integer cantidad, @SessionAttribute Pedido carrito) {
+		carritoService.guardarProductoEnCarrito(id, cantidad, carrito);
+
+		return "redirect:/carrito";
+	}
+
+	@GetMapping("/cerrar-sesion")
+	public String abandonarCarrito(SessionStatus status) {
+		status.setComplete();
+		return "redirect:/";
 	}
 }
